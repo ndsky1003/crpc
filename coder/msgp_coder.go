bd8d2cbe8653b1cf5c15c387ed01cf0d574aa897
cc <cc@1t.com>
2024-11-24 16:42:18 +0700

    fix

--- coder/msgp_coder.go
+++ coder/msgp_coder.go
@@ -1,6 +1,7 @@
 // 目前这个性能最好,不在乎其内部结构是否缺少
 // msgp 是msgpack的代码生成实现
-// vmihailenco/msgpack 是msgpack的非代码实现,这2个玩意儿是兼容的
+// vmihailenco/msgpack 是msgpack的非代码实现,这2个玩意儿是大体兼容的,在一些特殊处理下还是有区别:var a []string = nil,就无法混用
+// 大体上,之前遇到的不兼容的地方,都是编码使用msgpack,解码又用的是msgp
 package coder
 
 import (
@@ -19,6 +20,9 @@ func new_msgp_coder() *msgp_coder {
 }
 
 func (this *msgp_coder) Marshal(v any) ([]byte, error) {
+	if v == nil {
+		return []byte{192}, nil
+	}
 	buf := buffer.Get()
 	defer buf.Release()
 	if value, ok := v.(msgp.Encodable); ok {
@@ -27,25 +31,29 @@ func (this *msgp_coder) Marshal(v any) ([]byte, error) {
 		}
 		data := buf.Bytes()
 		return data, nil
-	} else {
-		enc := msgpack.GetEncoder()
-		defer msgpack.PutEncoder(enc)
-		enc.Reset(buf)
-		if err := enc.Encode(v); err != nil {
-			return nil, err
-		}
-		return buf.Bytes(), nil
 	}
+
+	enc := msgpack.GetEncoder()
+	defer msgpack.PutEncoder(enc)
+	enc.Reset(buf)
+	if err := enc.Encode(v); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 func (this *msgp_coder) Unmarshal(data []byte, v any) error {
-	buf := buffer.Get()
-	defer buf.Release()
+	if v == nil {
+		return nil
+	}
+	if len(data) == 1 && data[0] == 192 {
+		dec := msgpack.GetDecoder()
+		defer msgpack.PutDecoder(dec)
+		dec.Reset(bytes.NewReader(data))
+		return dec.Decode(v)
+	}
 	if value, ok := v.(msgp.Decodable); ok {
-		if _, err := buf.Write(data); err != nil {
-			return err
-		}
-		return msgp.Decode(buf, value)
+		return value.DecodeMsg(msgp.NewReader(bytes.NewReader(data)))
 	} else {
 		dec := msgpack.GetDecoder()
 		defer msgpack.PutDecoder(dec)
@@ -53,3 +61,30 @@ func (this *msgp_coder) Unmarshal(data []byte, v any) error {
 		return dec.Decode(v)
 	}
 }
+
+//支持下面的所有数据类型,msgp,以及0值,nil等
+// type Obj struct {
+// 	Name            string
+// 	TimeValue       time.Time
+// 	Data            []byte
+// 	Age             int
+// 	Height          float64
+// 	IsStudent       bool
+// 	Hobbies         []string
+// 	Attributes      map[string]interface{}
+// 	FavoriteNumbers [5]int
+// 	School          *School
+// 	Grades          []Grade
+// 	OptionalData    interface{}
+// 	UintValue       uint
+// 	Int8Value       int8
+// 	Int16Value      int16
+// 	Int32Value      int32
+// 	Int64Value      int64
+// 	Uint8Value      uint8
+// 	Uint16Value     uint16
+// 	Uint32Value     uint32
+// 	Uint64Value     uint64
+// 	ByteValue       byte
+// 	RuneValue       rune
+// }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ndsky1003/crpc/v2
 
-go 1.22
+go 1.22.0
 
 require (
 	github.com/golang/snappy v0.0.4
@@ -8,12 +8,12 @@ require (
 	github.com/ndsky1003/crpc v1.0.14
 	github.com/samber/lo v1.47.0
 	github.com/sirupsen/logrus v1.9.0
-	github.com/tinylib/msgp v1.1.9
+	github.com/tinylib/msgp v1.2.4
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 )
 
 require (
-	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
