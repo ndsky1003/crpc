4976b584c6b12ca8df0aff92205f90696245969a
cc <dd@cc.com>
2025-12-13 22:48:35 +0700

    fix

--- client/api.go
+++ client/api.go
@@ -44,7 +44,6 @@ func (c *Client) Send(ctx context.Context, service, method string, args any, opt
 
 func (c *Client) Close() error {
 	err := c.client.Close()
-	c.pending.Clear()
 	c.serviceMap.Clear()
 	for i := range c.handlers {
 		c.handlers[i] = nil
